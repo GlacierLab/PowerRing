b462c01e7673d999e5dc42134c317c063498506d
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2022-02-13 12:34:08 +0800

    gpu monitor

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PowerRing
 聚能环？！让有限的供电功率换取更好的游戏性能！
+
+
+## 开源依赖
+`本项目由以下开源项目驱动`  
+[.NET](https://github.com/dotnet)  
+[LibreHardwareMonitor](https://github.com/LibreHardwareMonitor/LibreHardwareMonitor)  
+
+
+## 工程名称为什么叫Resona
+[大藏里想奈(Resona Ookura)](https://zh.moegirl.org.cn/%E5%A4%A7%E8%97%8F%E9%87%8C%E6%83%B3%E5%A5%88):`喜欢玩网游，并在网游中是站在顶峰的人，例如全服务器仅有五把的EX-咖喱棒都被她买断了。（根据露娜的出价，一把至少是200W日元）`
--- Resona/MainWindow.xaml.vb
+++ Resona/MainWindow.xaml.vb
@@ -1,21 +1,51 @@
-﻿Imports System.Diagnostics
+﻿Imports LibreHardwareMonitor.Hardware
 
-Class MainWindow
-    Dim cpuCounter = New PerformanceCounter("Processor", "% Processor Time", "_Total", True)
+Public Class MainWindow
+    Dim _computeruCounter
+    Dim GPUPowerSensor
+    Dim CurrentGPU
 
     Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         StartMonitor()
     End Sub
     Private Function StartMonitor()
+        _computeruCounter = New PerformanceCounter("Processor", "% Processor Time", "_Total", True)
+        GPUFinder()
         Dim dispatcherTimer = New Threading.DispatcherTimer()
         AddHandler dispatcherTimer.Tick, AddressOf dispatcherTimer_Tick
         dispatcherTimer.Interval = New TimeSpan(0, 0, 1)
         dispatcherTimer.Start()
         Return True
     End Function
+    Private Sub GPUFinder()
+        Dim _computer = New Computer()
+        _computer.IsGpuEnabled = True
+        _computer.Open()
+        For i As Integer = 0 To _computer.Hardware.Count() - 1
+            If _computer.Hardware(i).HardwareType = HardwareType.GpuNvidia Then
+                CurrentGPU = _computer.Hardware(i)
+            End If
+        Next
+        If CurrentGPU IsNot Nothing Then
+            For i As Integer = 0 To CurrentGPU.Sensors.length() - 1
+                If CurrentGPU.Sensors(i).SensorType = SensorType.Power Then
+                    GPUPowerSensor = CurrentGPU.Sensors(i)
+                End If
+            Next
+            If GPUPowerSensor Is Nothing Then
+                MsgBox("不支持当前显卡，可能是驱动版本过低",, "错误")
+            End If
+        Else
+            MsgBox("未找到可检测的显卡",, "错误")
+        End If
+
+    End Sub
 
     Private Sub dispatcherTimer_Tick(sender As Object, e As EventArgs)
-        Dim value = cpuCounter.NextValue()
-        CPUPercent.Content = value.ToString() + "%"
+        Dim CPU = _computeruCounter.NextValue()
+        CPUPercent.Content = CPU.ToString() + "%"
+        CurrentGPU.Update()
+        Dim GPU = GPUPowerSensor.Value()
+        GPUPower.Content = GPU.ToString() + "W"
     End Sub
 End Class
