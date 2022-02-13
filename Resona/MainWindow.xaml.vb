1923635ce569fbd96579daebb2be63af208c8ff4
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2022-02-13 09:55:27 +0800

    cpu monitor

--- Resona/MainWindow.xaml.vb
+++ Resona/MainWindow.xaml.vb
@@ -1,3 +1,21 @@
-﻿Class MainWindow
+﻿Imports System.Diagnostics
 
+Class MainWindow
+    Dim cpuCounter = New PerformanceCounter("Processor", "% Processor Time", "_Total", True)
+
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        StartMonitor()
+    End Sub
+    Private Function StartMonitor()
+        Dim dispatcherTimer = New Threading.DispatcherTimer()
+        AddHandler dispatcherTimer.Tick, AddressOf dispatcherTimer_Tick
+        dispatcherTimer.Interval = New TimeSpan(0, 0, 1)
+        dispatcherTimer.Start()
+        Return True
+    End Function
+
+    Private Sub dispatcherTimer_Tick(sender As Object, e As EventArgs)
+        Dim value = cpuCounter.NextValue()
+        CPUPercent.Content = value.ToString() + "%"
+    End Sub
 End Class
