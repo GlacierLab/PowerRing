749043fbfa674810548d95a2c84d6eb7862b8f0e
琴梨梨OvO <24567775+qinlili23333@users.noreply.github.com>
2024-11-30 18:04:10 +0800

    禁止多个实例运行

--- Resona/Init.xaml.vb
+++ Resona/Init.xaml.vb
@@ -1,11 +1,46 @@
-﻿Public Class Init
+﻿Imports System.Threading
+
+Public Class Init
+
+    Private Shared _mutex As Mutex = Nothing
 
     Private Async Sub Init_ContentRendered(sender As Object, e As EventArgs) Handles Me.ContentRendered
+        Dim createdNew As Boolean
+        _mutex = New Mutex(True, "ProjectResona_PowerRing", createdNew)
+        If Not createdNew Then
+            Status.Content = "无法同时运行多个实例" + Environment.NewLine + "点击继续并关闭其他实例"
+            Status.Foreground = New SolidColorBrush(ColorConverter.ConvertFromString("#FFFF0000"))
+            BorderBrush = New SolidColorBrush(ColorConverter.ConvertFromString("#FFFF0000"))
+            ShowInTaskbar = True
+            Title = "初始化失败"
+            AddHandler MouseLeftButtonDown, AddressOf KillAndReload
+        Else
+            Await LoadMain()
+        End If
+
+    End Sub
+
+    Private Sub KillAndReload(sender As Object, e As MouseButtonEventArgs)
+        Dim Proc = Process.GetProcessesByName("Resona")
+        If Proc.Length > 0 Then
+            For Each Pro In Proc
+                If Not Pro.Id Like Process.GetCurrentProcess().Id Then
+                    Pro.Kill()
+                End If
+            Next
+        End If
+        _mutex.Dispose()
+        Dim NewInit As New Init()
+        NewInit.Show()
+        Close()
+    End Sub
+
+    Private Async Function LoadMain() As Task
         Dim Main As MainWindow = New MainWindow()
         Await Main.PreInit()
         Main.Show()
         Await Task.Delay(250)
         Close()
-    End Sub
+    End Function
 
 End Class
