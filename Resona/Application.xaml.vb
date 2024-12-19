8b761da805936bba44204db088b55540d734a1b9
琴梨梨OvO <24567775+qinlili23333@users.noreply.github.com>
2024-12-19 14:04:33 +0800

    出错自动生成日志

--- Resona/Application.xaml.vb
+++ Resona/Application.xaml.vb
@@ -1,4 +1,15 @@
-﻿Class Application
+﻿Imports System.IO
+Imports System.Windows.Threading
+
+Class Application
+    Private Sub Application_DispatcherUnhandledException(sender As Object, e As DispatcherUnhandledExceptionEventArgs) Handles Me.DispatcherUnhandledException
+
+        MessageBox.Show("聚能环遇到无法处理的错误，即将在浏览器内打开GitHub提交错误")
+        Dim startInfo As New ProcessStartInfo("https://github.com/GlacierLab/PowerRing/issues/new?title=" + e.Exception.GetType().ToString() + "&body=" + e.Exception.Message + e.Exception.StackTrace.Replace(" at", "%0Aat") + "&labels=bug")
+        startInfo.UseShellExecute = True
+        Process.Start(startInfo)
+        e.Handled = True
+    End Sub
 
     ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
     ' can be handled in this file.
